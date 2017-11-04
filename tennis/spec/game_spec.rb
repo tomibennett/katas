3be445b6700300d8969bc2799c516391e51c0244
Yuhiba <elodie.pelcat@gmail.com>
2017-11-04 19:02:22 +0100

    refactor(game): remove unused spec

--- tennis/spec/game_spec.rb
+++ tennis/spec/game_spec.rb
@@ -2,10 +2,6 @@ require_relative '../src/game'
 
 RSpec.describe Game do
   describe 'games points rules' do
-    it 'has five points' do
-      expect(described_class::POINTS.keys.length).to eq 5
-    end
-
     it 'has the first point set to "love"' do
       expect(described_class::POINTS[0]).to eq 'love'
     end
