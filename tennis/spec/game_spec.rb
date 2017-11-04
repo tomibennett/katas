7a39694cc946329e6c355ea5016add180539eecb
Yuhiba <elodie.pelcat@gmail.com>
2017-11-04 18:56:10 +0100

    feat(game): added point 2 correspondance

--- tennis/spec/game_spec.rb
+++ tennis/spec/game_spec.rb
@@ -13,5 +13,9 @@ RSpec.describe Game do
     it 'has the second point set to "15"' do
       expect(described_class::POINTS[1]).to eq '15'
     end
+
+    it 'has the third point set to "30"' do
+      expect(described_class::POINTS[2]).to eq '30'
+    end
   end
 end
