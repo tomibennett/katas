440a4a0bd8f11df86a8082970fa22ab30fe61793
tbenet <tom@benet.io>
2017-11-04 18:47:51 +0100

    feat(game): added point 0 correspondance

--- tennis/spec/game_spec.rb
+++ tennis/spec/game_spec.rb
@@ -2,8 +2,12 @@ require_relative '../src/game'
 
 RSpec.describe Game do
   describe 'games scoring rules' do
-    it 'has four points' do
+    it 'has five points' do
       expect(described_class::SCORING_RULES.keys.length).to eq 5
+    end
+
+    it 'has the first point set to "love"' do
+      expect(described_class::SCORING_RULES[0]).to eq 'love'
     end
   end
 end
