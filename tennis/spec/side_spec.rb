2a70c0881f63bd90c189bf859333e01df5a975cb
tbenet <tom@benet.io>
2017-11-07 18:54:52 +0100

    feat(side): score accumulates points

--- tennis/spec/side_spec.rb
+++ tennis/spec/side_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe Side do
       expect(described_class.new.points).to eq 0
     end
   end
+
+  describe '#score' do
+    it 'increments the total of points by one' do
+      side = described_class.new
+      side.score
+
+      expect(side.points).to eq 1
+    end
+  end
 end
