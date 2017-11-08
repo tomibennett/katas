5f1e5fe65c4afb020fd979179c712bba48a04e24
Yuhiba <elodie.pelcat@gmail.com>
2017-11-08 18:02:28 +0100

    feat(point_stream) can retrieve firsts two points

--- tennis/spec/points_stream_spec.rb
+++ tennis/spec/points_stream_spec.rb
@@ -17,4 +17,28 @@ RSpec.describe PointsStream do
       expect(points_stream.register_judge judge).to eq judge
     end
   end
+
+  describe '#get_point' do
+    context 'at the start of the game' do
+      let(:points) { '010101' }
+      let(:points_stream) { described_class.from_buffer points }
+
+      it 'returns the first point at the start of the game' do
+        expect(points_stream.get_point).to eq points[0]
+      end
+    end
+
+    context 'at the second round of the game' do
+      let(:points) { '010101' }
+      let(:points_stream) { described_class.from_buffer points }
+
+      before do
+        points_stream.get_point
+      end
+
+      it 'returns the second point' do
+        expect(points_stream.get_point).to eq points[1]
+      end
+    end
+  end
 end
--- tennis/src/points_stream.rb
+++ tennis/src/points_stream.rb
@@ -1,6 +1,7 @@
 class PointsStream
   def initialize points
     @points = points
+    @current_point_index = 0
   end
 
   def self.from_buffer points
@@ -9,5 +10,12 @@ class PointsStream
 
   def register_judge(judge)
     @judge = judge
+  end
+
+  def get_point
+    point = @points[@current_point_index]
+
+    @current_point_index += 1
+    point
   end
 end
