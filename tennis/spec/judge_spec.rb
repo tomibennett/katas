ae6dbbbaa83ba140103cebc5730a39b8787981f2
Yuhiba <elodie.pelcat@gmail.com>
2017-11-08 13:07:55 +0100

    feat(judge): can retrieve the first point

--- tennis/spec/judge_spec.rb
+++ tennis/spec/judge_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Judge do
       expect(judge.game_stream).to eq game_stream
     end
   end
+
+  describe '#next_point' do
+    let(:judge) { described_class.new }
+
+    before do
+      judge.subscribes game_stream
+    end
+
+    it 'retrieves a point won by a side' do
+      expect(judge.next_point).to eq '0'
+    end
+  end
 end
