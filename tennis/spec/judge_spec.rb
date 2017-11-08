e4fc28d9ad61d26ddcdea9c00a0fed2613abf846
tbenet <tom@benet.io>
2017-11-08 12:37:23 +0100

    feat(judge): subscribe to the game

--- tennis/spec/judge_spec.rb
+++ tennis/spec/judge_spec.rb
@@ -1,7 +1,14 @@
 require_relative '../src/judge'
 
 RSpec.describe Judge do
-  it 'is true' do
-    expect(true).to eq true
+  let(:game_stream) { GameStream.from_buffer '01' }
+
+  describe '#subscribes' do
+    it 'subscribes to a game stream' do
+      judge = described_class.new
+      judge.subscribes game_stream
+
+      expect(judge.game_stream).to eq game_stream
+    end
   end
 end
