9a0c2c0080b00c4d087724beafe009438f82b5c0
tbenet <tom@benet.io>
2017-11-08 17:17:47 +0100

    refactor(GameStream): stream of points renamed to PointsStream

--- tennis/spec/points_stream_spec.rb
+++ tennis/spec/points_stream_spec.rb
@@ -1,9 +1,9 @@
-require_relative '../src/game_stream'
+require_relative '../src/points_stream'
 
-RSpec.describe GameStream do
+RSpec.describe PointsStream do
   describe '.from_buffer' do
     context 'reads score from a points buffer' do
-      it 'instantiates a new game stream' do
+      it 'instantiates a new points stream' do
         expect(described_class.from_buffer '').to be_a described_class
       end
     end
