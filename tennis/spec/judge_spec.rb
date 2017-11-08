195b3de421509f4c3ba48eda46cfe9310f571048
tbenet <tom@benet.io>
2017-11-08 17:25:49 +0100

    refactor(judge): judge is not responsible to subscription to points stream

--- tennis/spec/judge_spec.rb
+++ tennis/spec/judge_spec.rb
@@ -1,24 +1,5 @@
 require_relative '../src/judge'
 
 RSpec.describe Judge do
-  let(:points_stream) { PointsStream.from_buffer '01' }
 
-  describe '#subscribes' do
-    let(:judge) { described_class.new }
-    it 'subscribes to a points stream' do
-      expect(judge.subscribes points_stream).to eq points_stream
-    end
-  end
-
-  describe '#next_point' do
-    let(:judge) { described_class.new }
-
-    before do
-      judge.subscribes points_stream
-    end
-
-    it 'retrieves a point won by a side' do
-      expect(judge.next_point).to eq '0'
-    end
-  end
 end
--- tennis/src/judge.rb
+++ tennis/src/judge.rb
@@ -1,11 +1,2 @@
-require_relative './points_stream'
-
 class Judge
-  def subscribes(points_stream)
-    @points_stream = points_stream
-  end
-
-  def next_point
-    '0'
-  end
 end
